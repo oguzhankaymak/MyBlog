6de733438f25fc67b24c86db074cf48618270e2b
Oğuzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-04-11 12:00:56 +0300

    next-mdx add for blogs

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,37 @@
+import { getMdxNode, getMdxPaths } from 'next-mdx/server'
+
+export default function PostPage({ post }) {
+  return (
+    <div className="site-container">
+      <article>
+        <h1 className="text-4xl font-bold">{post.frontMatter.title}</h1>
+        <p>{post.frontMatter.excerpt}</p>
+        <hr className="my-4" />
+        <div className="prose">{post.content}</div>
+      </article>
+    </div>
+  )
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: await getMdxPaths('post'),
+    fallback: false
+  }
+}
+
+export async function getStaticProps(context) {
+  const post = await getMdxNode('post', context)
+
+  if (!post) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
+}
---
+++ pages/blog/index.js
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import { getAllNodes } from 'next-mdx/server'
+
+function BlogPage({ posts }) {
+  return (
+    <div className="site-container">
+      <div className="space-y-4">
+        {posts.map((post) => (
+          <article key={post.url}>
+            <h2 className="text-xl font-bold">
+              <Link href={post.url}>
+                <a>{post.frontMatter.title}</a>
+              </Link>
+            </h2>
+            <p>{post.frontMatter.excerpt}</p>
+            <div className="text-gray-400">
+              <span>{post.frontMatter.date}</span>
+            </div>
+          </article>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      posts: await getAllNodes('post')
+    }
+  }
+}
+
+export default BlogPage
