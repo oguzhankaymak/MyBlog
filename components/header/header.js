b653a3782cefd75ab18e730dcbbdb673281bb155
Oğuzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-04-16 12:21:53 +0300

    dark mode change media

--- components/header/header.js
+++ components/header/header.js
@@ -1,17 +1,6 @@
 import Link from 'next/link'
 
 export default function Header() {
-  const changeTheme = () => {
-    let htmlClasses = document.querySelector('html').classList
-    if (localStorage.theme === 'dark') {
-      htmlClasses.remove('dark')
-      localStorage.removeItem('theme')
-    } else {
-      htmlClasses.add('dark')
-      localStorage.theme = 'dark'
-    }
-  }
-
   return (
     <header className="flex flex-row justify-between items-center site-container py-6">
       <nav className="space-x-4">
@@ -25,9 +14,6 @@ export default function Header() {
           <a>Blog</a>
         </Link>
       </nav>
-      <button className="focus:outline-none" onClick={changeTheme}>
-        <img src="/light.svg" height={28} width={28} alt="change theme" />
-      </button>
     </header>
   )
 }
