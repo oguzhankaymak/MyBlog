cd60a66499463ff61647cf805c4be98e286311d9
Oğuzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-04-22 01:44:05 +0300

    comment component create

---
+++ components/comments/comments.js
@@ -0,0 +1,33 @@
+import { DateTime } from 'luxon'
+
+const Comments = ({ comments }) => {
+  return (
+    <div className="mt-10 space-y-4">
+      {comments.map(({ id, createdTime, text, user }) => {
+        return (
+          <div key={id} className="flex items-center space-x-2">
+            <div>
+              <img
+                src={user.picture}
+                alt={user.name}
+                width={40}
+                className="rounded-full"
+              />
+            </div>
+            <div>
+              <div className="space-x-2">
+                <b>{user.name}</b>
+                <time className="text-gray-500">
+                  {DateTime.fromMillis(createdTime).toRelative()}
+                </time>
+              </div>
+              <p>{text}</p>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Comments
