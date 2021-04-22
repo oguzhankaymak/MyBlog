4c3ac04a6335a974a7765ebd62173db6a7a76214
Oğuzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-04-22 23:05:22 +0300

    syntax highlighter implement

--- components/mdxComponents/mdx-componets.js
+++ components/mdxComponents/mdx-componets.js
@@ -1,3 +1,6 @@
+import Pre from '../pre/pre'
+
 export const mdxComponents = {
-  h2: (props) => <h2 style={{ color: 'gray' }} {...props} />
+  h2: (props) => <h2 style={{ color: 'gray' }} {...props} />,
+  pre: ({ children }) => <Pre {...children.props} />
 }
---
+++ components/pre/pre.js
@@ -0,0 +1,13 @@
+import { Prism } from 'react-syntax-highlighter'
+import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+const Pre = ({ children, className }) => {
+  const lang = className.split('-')[1]
+  return (
+    <Prism language={lang} style={materialDark}>
+      {children}
+    </Prism>
+  )
+}
+
+export default Pre
